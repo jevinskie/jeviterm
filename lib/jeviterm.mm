cbce74157f23adbc044979ae7899284bf5050f94
Jevin Sweval <jevinsweval@gmail.com>
2022-01-12 18:42:39 -0800

    ahh good ol C++

---
+++ lib/jeviterm.mm
@@ -0,0 +1,7 @@
+#include "jeviterm.h"
+
+#import <Foundation/Foundation.h>
+
+void doit(void) {
+    NSLog(@"Just Do It");
+}
---
+++ tools/jevitermtest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include <jeviterm.h>
+
+int main(void) {
+    printf("noodlin to do the doodlin\n");
+    doit();
+    printf("doit done diddit\n");
+    return 0;
+}
