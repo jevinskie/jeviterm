69114d7ba307555ae85e425741c838c28f5efa2b
Jevin Sweval <jevinsweval@gmail.com>
2022-01-14 11:54:17 -0800

    cool it works

--- lib/jeviterm.mm
+++ lib/jeviterm.mm
@@ -23,56 +23,48 @@ namespace websocket = beast::websocket;              // from <boost/beast/websoc
 namespace net       = boost::asio;                   // from <boost/asio.hpp>
 using unix_fd = boost::asio::local::stream_protocol; // from <boost/asio/local/stream_protocol.hpp>
 
-
-static void hexdump(void *buf, std::size_t sz) {
-    for (const uint8_t *p = (const uint8_t *)buf; p < (const uint8_t *)buf + sz; ++p) {
-        std::cerr << std::setfill('0') << std::setw(2) << std::hex << (unsigned int)*p;
-    }
-}
-
 class iTermRPC {
 public:
-    iTermRPC(std::string client_name) : m_client_name{client_name}, m_ws{m_ioc}, m_ep{getSocketPath()} {
+    iTermRPC(std::string client_name)
+        : m_client_name{client_name}, m_ws{m_ioc}, m_ep{getSocketPath()} {
         m_ws.binary(true);
-        if (!connect()) {
-        }
+        connect();
     }
 
     ~iTermRPC() {
-        disconnect();
+        // FIXME: clean disconnect
+        // disconnect();
     }
 
-    bool create_tab(const char *cmd) {
+    using window_id_t = std::string;
+
+    std::optional<window_id_t> create_tab(const char *cmd, const window_id_t *window = nullptr) {
         assert(cmd);
 
-        const auto reqMsg = get_create_tab_msg(cmd);
+        const auto reqMsg     = get_create_tab_msg(cmd, window);
         const auto out_msg_sz = reqMsg.ByteSizeLong();
         beast::flat_buffer out_msg_buf{out_msg_sz};
         reqMsg.SerializeToArray(out_msg_buf.prepare(out_msg_sz).data(), out_msg_sz);
         out_msg_buf.commit(out_msg_sz);
 
-        std::cerr << "write begin\n";
         // Send the message
-        // ws.write(beast::flat_buffer{out_msg_buf.data(), out_msg_buf.size()});
         m_ws.write(out_msg_buf.data());
-        std::cerr << "write end\n";
 
         // This buffer will hold the incoming message
         beast::flat_buffer in_msg_buf;
 
         // Read a message into our buffer
-        std::cerr << "read begin\n";
         m_ws.read(in_msg_buf);
-        std::cerr << "read end\n";
-        std::cerr << "read n bytes: " << in_msg_buf.size() << "\n";
 
+        // validate
         iterm2::ServerOriginatedMessage replyMsg;
         replyMsg.ParseFromArray(in_msg_buf.data().data(), in_msg_buf.size());
-        std::cerr << "replyMsg: " << replyMsg.DebugString() << "\n";
-        if (!replyMsg.has_create_tab_response() || replyMsg.create_tab_response().status() != iterm2::CreateTabResponse_Status_OK) {
-            return false;
+        if (!replyMsg.has_create_tab_response() || !replyMsg.create_tab_response().has_status() ||
+            replyMsg.create_tab_response().status() != iterm2::CreateTabResponse_Status_OK ||
+            !replyMsg.create_tab_response().has_window_id()) {
+            return std::nullopt;
         }
-        return true;
+        return {replyMsg.create_tab_response().window_id()};
     }
 
     struct CookieKey {
@@ -80,28 +72,30 @@ public:
         std::string key;
     };
 
-    static const std::optional<CookieKey> &getCookieAndKey(std::string clientName, bool force = false) {
+    static const std::optional<CookieKey> &getCookieAndKey(std::string clientName,
+                                                           bool force = false) {
         static std::optional<CookieKey> cookieKey{std::nullopt};
         if (!cookieKey || force) {
             NSDictionary<NSString *, id> *err = nil;
             NSAppleScript *get_stuff_as       = [[NSAppleScript alloc]
-                initWithSource:[NSString
-                                   stringWithFormat:@"tell application \"iTerm2\" to request cookie "
-                                                          @"and key for app named \"%s\"",
-                                                    clientName.c_str()]];
+                initWithSource:
+                    [NSString stringWithFormat:@"tell application \"iTerm2\" to request cookie "
+                                                     @"and key for app named \"%s\"",
+                                               clientName.c_str()]];
             NSAppleEventDescriptor *res_evt   = [get_stuff_as executeAndReturnError:&err];
             if (err) {
-                std::cerr << "AppleScript error: " << [NSString stringWithFormat:@"%@", err].UTF8String << "\n";
+                std::cerr << "jeviterm AppleScript error: " <<
+                    [NSString stringWithFormat:@"%@", err].UTF8String << "\n";
                 return cookieKey;
             }
-            NSArray<NSString *> *splitParts = [res_evt.stringValue componentsSeparatedByString:@" "];
+            NSArray<NSString *> *splitParts =
+                [res_evt.stringValue componentsSeparatedByString:@" "];
             assert(splitParts.count == 2);
             cookieKey = CookieKey{.cookie = std::string{splitParts[0].UTF8String},
                                   .key    = std::string{splitParts[1].UTF8String}};
         }
         return cookieKey;
     }
-
 
     static std::string getSocketPath(void) {
         return std::string{[[[NSFileManager.defaultManager
@@ -118,10 +112,7 @@ private:
             return false;
         }
 
-        std::cerr << "connecting\n";
-        // Make the connection on the IP address we get from a lookup
         boost::beast::get_lowest_layer(m_ws).connect(m_ep);
-        std::cerr << "connected\n";
 
         // Set a decorator to change the User-Agent of the handshake
         m_ws.set_option(websocket::stream_base::decorator([&](websocket::request_type &req) {
@@ -140,14 +131,13 @@ private:
     }
 
     bool disconnect() {
-        std::cerr << "close begin\n";
-        // Close the WebSocket connection
+        // FIXME: throws even on good nominal disconnect?
         m_ws.close(websocket::close_code::normal);
-        std::cerr << "close end\n";
         return true;
     }
 
-    iterm2::ClientOriginatedMessage get_create_tab_msg(std::string cmd) {
+    iterm2::ClientOriginatedMessage get_create_tab_msg(std::string cmd,
+                                                       const window_id_t *window = nullptr) {
         iterm2::ClientOriginatedMessage reqMsg;
 
         iterm2::CreateTabRequest ctReqMsg;
@@ -157,6 +147,9 @@ private:
         auto cmdProp = ctReqMsg.add_custom_profile_properties();
         cmdProp->set_key("Command");
         cmdProp->set_json_value("\"" + cmd + "\"");
+        if (window) {
+            ctReqMsg.set_window_id(*window);
+        }
         *reqMsg.mutable_create_tab_request() = ctReqMsg;
 
         return reqMsg;
@@ -171,8 +164,10 @@ private:
 
 } // namespace jeviterm
 
-__attribute__((visibility("default")))
-int jeviterm_open_tabs(const char **cmds, const char *client_name) {
+using namespace jeviterm;
+
+__attribute__((visibility("default"))) int jeviterm_open_tabs(const char **cmds, int same_window,
+                                                              const char *client_name) {
     bool good = true;
     if (!cmds) {
         return 1;
@@ -181,20 +176,21 @@ int jeviterm_open_tabs(const char **cmds, const char *client_name) {
         client_name = "jeviterm";
     }
     try {
-        jeviterm::iTermRPC rpc{client_name};
+        iTermRPC rpc{client_name};
+        iTermRPC::window_id_t existing_window_id;
         for (const char **cmdp = cmds; *cmdp != nullptr; ++cmdp) {
-            good &= rpc.create_tab(*cmdp);
+            const auto new_window_id =
+                rpc.create_tab(*cmdp, existing_window_id.empty() ? nullptr : &existing_window_id);
+            good &= new_window_id.has_value();
+            if (new_window_id && same_window) {
+                existing_window_id = *new_window_id;
+            }
         }
     } catch (std::exception const &e) {
-        std::cerr << "Error: " << e.what() << "\n";
+        std::cerr << "jeviterm_open_tabs error: " << e.what() << "\n";
         return 1;
     }
     return !good; // 0 on success
-}
-
-__attribute__((visibility("default"))) void doit(void) {
-    const char *cmds[] = {"/usr/bin/env bash -l -c vi", "/usr/bin/env bash -l -c nano", nullptr};
-    jeviterm_open_tabs(cmds, "jevitermtest");
 }
 
 __attribute__((visibility("default"))) const char *jeviterm_version(void) {
@@ -214,4 +210,3 @@ __attribute__((visibility("default"))) const char *jeviterm_version(void) {
     }
     return res.c_str();
 }
-
--- tools/jevitermctl.mm
+++ tools/jevitermctl.mm
@@ -1,11 +1,15 @@
 #include <cstdio>
+#include <cstring>
 
 #include <jeviterm.h>
 
-int main(void) {
-    printf("noodlin to do the doodlin\n");
-    puts(jeviterm_version());
-    doit();
-    printf("doit done diddit\n");
-    return 0;
+int main(int argc, const char **argv) {
+    if (argc < 2) {
+        printf("Usage: jevitermctl <tab 1 command str> <tab 2 command str> ...\n");
+        return -1;
+    }
+    const char *cmds[argc];
+    cmds[argc - 1] = nullptr;
+    memcpy(cmds, argv + 1, (argc - 1) * sizeof(*cmds));
+    return jeviterm_open_tabs(cmds, true, "jevitermctl");
 }
