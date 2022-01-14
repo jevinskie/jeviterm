4ea46be53f3425e4d2e278adb221f28877a36791
Jevin Sweval <jevinsweval@gmail.com>
2022-01-14 10:37:17 -0800

    a real api

--- lib/jeviterm.mm
+++ lib/jeviterm.mm
@@ -13,44 +13,16 @@
 #include <optional>
 #include <string>
 
+using namespace std::string_literals;
+
+namespace jeviterm {
+
 namespace beast     = boost::beast;                  // from <boost/beast.hpp>
 namespace http      = beast::http;                   // from <boost/beast/http.hpp>
 namespace websocket = beast::websocket;              // from <boost/beast/websocket.hpp>
 namespace net       = boost::asio;                   // from <boost/asio.hpp>
 using unix_fd = boost::asio::local::stream_protocol; // from <boost/asio/local/stream_protocol.hpp>
 
-struct CookieKey {
-    std::string cookie;
-    std::string key;
-};
-
-static const std::optional<CookieKey> &getCookieAndKey(std::string clientName, bool force = false) {
-    static std::optional<CookieKey> cookieKey{std::nullopt};
-    if (!cookieKey || force) {
-        NSDictionary<NSString *, id> *err = nil;
-        NSAppleScript *get_stuff_as       = [[NSAppleScript alloc]
-            initWithSource:[NSString stringWithFormat:@"tell application \"iTerm2\" to request cookie "
-                                                            @"and key for app named \"%s\"",
-                                                      clientName.c_str()]];
-        NSAppleEventDescriptor *res_evt   = [get_stuff_as executeAndReturnError:&err];
-        if (err) {
-            NSLog(@"AppleScript error: %@", err);
-            return cookieKey;
-        }
-        NSArray<NSString *> *splitParts = [res_evt.stringValue componentsSeparatedByString:@" "];
-        assert(splitParts.count == 2);
-        cookieKey = CookieKey{.cookie = std::string{splitParts[0].UTF8String},
-                         .key    = std::string{splitParts[1].UTF8String}};
-    }
-    return cookieKey;
-}
-
-static std::string getSocketPath(void) {
-    return std::string{[[[NSFileManager.defaultManager
-               URLsForDirectory:NSApplicationSupportDirectory
-                      inDomains:NSUserDomainMask][0] path] UTF8String]} +
-           "/iTerm2/private/socket";
-}
 
 static void hexdump(void *buf, std::size_t sz) {
     for (const uint8_t *p = (const uint8_t *)buf; p < (const uint8_t *)buf + sz; ++p) {
@@ -58,54 +30,125 @@ static void hexdump(void *buf, std::size_t sz) {
     }
 }
 
-
-
-// Sends a WebSocket message and prints the response
-int demo_main(std::string clientName) {
-    const auto cookieKey = getCookieAndKey(clientName);
-    if (!cookieKey) {
-        std::cerr << "failed to get cookie and key from AppleScript\n";
-        return -1;
+class iTermRPC {
+public:
+    iTermRPC(std::string client_name) : m_client_name{client_name}, m_ws{m_ioc}, m_ep{getSocketPath()} {
+        m_ws.binary(true);
+        if (!connect()) {
+        }
     }
-    std::cerr << "cookie: " << cookieKey->cookie << " key: " << cookieKey->key << "\n";
-    try {
-        // The io_context is required for all I/O
-        net::io_context ioc;
 
-        // These objects perform our I/O
-        const auto sockPath = getSocketPath();
-        std::cerr << "sockPath: " << sockPath << "\n";
-        auto ep = unix_fd::endpoint{sockPath};
-        websocket::stream<unix_fd::socket> ws{ioc};
-        std::cerr << "ws obj created\n";
-        ws.binary(true);
+    ~iTermRPC() {
+        disconnect();
+    }
 
+    bool create_tab(const char *cmd) {
+        assert(cmd);
+
+        const auto reqMsg = get_create_tab_msg(cmd);
+        const auto out_msg_sz = reqMsg.ByteSizeLong();
+        beast::flat_buffer out_msg_buf{out_msg_sz};
+        reqMsg.SerializeToArray(out_msg_buf.prepare(out_msg_sz).data(), out_msg_sz);
+        out_msg_buf.commit(out_msg_sz);
+
+        std::cerr << "write begin\n";
+        // Send the message
+        // ws.write(beast::flat_buffer{out_msg_buf.data(), out_msg_buf.size()});
+        m_ws.write(out_msg_buf.data());
+        std::cerr << "write end\n";
+
+        // This buffer will hold the incoming message
+        beast::flat_buffer in_msg_buf;
+
+        // Read a message into our buffer
+        std::cerr << "read begin\n";
+        m_ws.read(in_msg_buf);
+        std::cerr << "read end\n";
+        std::cerr << "read n bytes: " << in_msg_buf.size() << "\n";
+
+        iterm2::ServerOriginatedMessage replyMsg;
+        replyMsg.ParseFromArray(in_msg_buf.data().data(), in_msg_buf.size());
+        std::cerr << "replyMsg: " << replyMsg.DebugString() << "\n";
+        if (!replyMsg.has_create_tab_response() || replyMsg.create_tab_response().status() != iterm2::CreateTabResponse_Status_OK) {
+            return false;
+        }
+        return true;
+    }
+
+    struct CookieKey {
+        std::string cookie;
+        std::string key;
+    };
+
+    static const std::optional<CookieKey> &getCookieAndKey(std::string clientName, bool force = false) {
+        static std::optional<CookieKey> cookieKey{std::nullopt};
+        if (!cookieKey || force) {
+            NSDictionary<NSString *, id> *err = nil;
+            NSAppleScript *get_stuff_as       = [[NSAppleScript alloc]
+                initWithSource:[NSString
+                                   stringWithFormat:@"tell application \"iTerm2\" to request cookie "
+                                                          @"and key for app named \"%s\"",
+                                                    clientName.c_str()]];
+            NSAppleEventDescriptor *res_evt   = [get_stuff_as executeAndReturnError:&err];
+            if (err) {
+                std::cerr << "AppleScript error: " << [NSString stringWithFormat:@"%@", err].UTF8String << "\n";
+                return cookieKey;
+            }
+            NSArray<NSString *> *splitParts = [res_evt.stringValue componentsSeparatedByString:@" "];
+            assert(splitParts.count == 2);
+            cookieKey = CookieKey{.cookie = std::string{splitParts[0].UTF8String},
+                                  .key    = std::string{splitParts[1].UTF8String}};
+        }
+        return cookieKey;
+    }
+
+
+    static std::string getSocketPath(void) {
+        return std::string{[[[NSFileManager.defaultManager
+                   URLsForDirectory:NSApplicationSupportDirectory
+                          inDomains:NSUserDomainMask][0] path] UTF8String]} +
+               "/iTerm2/private/socket";
+    }
+
+private:
+    bool connect() {
+        const auto cookieKey = getCookieAndKey(m_client_name);
+        if (!cookieKey) {
+            std::cerr << "failed to get cookie and key from AppleScript\n";
+            return false;
+        }
+
+        std::cerr << "connecting\n";
         // Make the connection on the IP address we get from a lookup
-        boost::beast::get_lowest_layer(ws).connect(ep);
+        boost::beast::get_lowest_layer(m_ws).connect(m_ep);
         std::cerr << "connected\n";
 
         // Set a decorator to change the User-Agent of the handshake
-        ws.set_option(websocket::stream_base::decorator([&](websocket::request_type &req) {
+        m_ws.set_option(websocket::stream_base::decorator([&](websocket::request_type &req) {
             req.set("origin", "ws://localhost/");
             req.set("host", "localhost");
-            req.set("x-iterm2-library-version", "python 0.24");
-            req.set("x-iterm2-disable-auth-ui", "true");
+            req.set("x-iterm2-library-version", "jeviterm 0.24");
+            req.set("x-iterm2-disable-auth-ui", "false"); // FIXME: does this do anything?
             req.set("x-iterm2-cookie", cookieKey->cookie);
             req.set("x-iterm2-key", cookieKey->key);
-            req.set("x-iterm2-advisory-name", clientName);
+            req.set("x-iterm2-advisory-name", m_client_name);
         }));
 
         // Perform the websocket handshake
-        ws.handshake("api.iterm2.com", "/");
+        m_ws.handshake("api.iterm2.com", "/");
+        return true;
+    }
 
+    bool disconnect() {
+        std::cerr << "close begin\n";
+        // Close the WebSocket connection
+        m_ws.close(websocket::close_code::normal);
+        std::cerr << "close end\n";
+        return true;
+    }
+
+    iterm2::ClientOriginatedMessage get_create_tab_msg(std::string cmd) {
         iterm2::ClientOriginatedMessage reqMsg;
-
-        // iterm2::FocusRequest focReqMsg;
-        // reqMsg.set_allocated_focus_request(&focReqMsg);
-
-        // iterm2::ProfileProperty cmdProp;
-        // cmdProp.set_key("Command");
-        // cmdProp.set_json_value("/usr/bin/env bash -l -c vi");
 
         iterm2::CreateTabRequest ctReqMsg;
         auto cstCmdProp = ctReqMsg.add_custom_profile_properties();
@@ -113,81 +156,60 @@ int demo_main(std::string clientName) {
         cstCmdProp->set_json_value("\"Yes\"");
         auto cmdProp = ctReqMsg.add_custom_profile_properties();
         cmdProp->set_key("Command");
-        cmdProp->set_json_value("\"/usr/bin/env bash -l -c vi\"");
-        // ctReqMsg.set_command("/usr/bin/env bash -l -c vi");
+        cmdProp->set_json_value("\"" + cmd + "\"");
         *reqMsg.mutable_create_tab_request() = ctReqMsg;
 
-        const auto out_msg_sz = reqMsg.ByteSizeLong();
-        // std::vector<uint8_t> out_msg_buf;
-        // out_msg_buf.resize(out_msg_sz);
-        // reqMsg.SerializeToArray(out_msg_buf.data(), out_msg_buf.size());
-        beast::flat_buffer out_msg_buf{out_msg_sz};
-        reqMsg.SerializeToArray(out_msg_buf.prepare(out_msg_sz).data(), out_msg_sz);
-        out_msg_buf.commit(out_msg_sz);
-        std::cerr << "out_msg_sz: " << out_msg_sz << " buf sz: " << out_msg_buf.size() << "\n";
-        std::cerr << "out_msg_buf: ";
-        hexdump(out_msg_buf.data().data(), out_msg_buf.size());
-        std::cerr << "\n";
-
-        // return 0;
-
-        std::cerr << "write begin\n";
-        // Send the message
-        // ws.write(beast::flat_buffer{out_msg_buf.data(), out_msg_buf.size()});
-        ws.write(out_msg_buf.data());
-        std::cerr << "write end\n";
-
-        // This buffer will hold the incoming message
-        beast::flat_buffer buffer;
-
-        for (int loopnum = 0; loopnum < 1; ++loopnum) {
-            // Read a message into our buffer
-            std::cerr << "read begin\n";
-            ws.read(buffer);
-            std::cerr << "read end\n";
-            std::cout << "read n bytes: " << buffer.size() << "\n";
-
-            // The make_printable() function helps print a ConstBufferSequence
-            // std::cout << beast::make_printable(buffer.data()) << "\n";
-        }
-
-        // sleep(5);
-
-        std::cerr << "close begin\n";
-        // Close the WebSocket connection
-        ws.close(websocket::close_code::normal);
-        std::cerr << "close end\n";
-
-        // If we get here then the connection is closed gracefully
-
-        // The make_printable() function helps print a ConstBufferSequence
-        // std::cout << beast::make_printable(buffer.data()) << "\n";
-    } catch (std::exception const &e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return EXIT_FAILURE;
+        return reqMsg;
     }
-    return EXIT_SUCCESS;
-}
+
+private:
+    net::io_context m_ioc;
+    unix_fd::endpoint m_ep;
+    websocket::stream<unix_fd::socket> m_ws;
+    std::string m_client_name;
+};
+
+} // namespace jeviterm
 
 __attribute__((visibility("default")))
-void doit(void) {
-    NSLog(@"Just Do It");
-    demo_main("jevitermtest");
+int jeviterm_open_tabs(const char **cmds, const char *client_name) {
+    bool good = true;
+    if (!cmds) {
+        return 1;
+    }
+    if (client_name == nullptr) {
+        client_name = "jeviterm";
+    }
+    try {
+        jeviterm::iTermRPC rpc{client_name};
+        for (const char **cmdp = cmds; *cmdp != nullptr; ++cmdp) {
+            good &= rpc.create_tab(*cmdp);
+        }
+    } catch (std::exception const &e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
+    }
+    return !good; // 0 on success
 }
 
-__attribute__((visibility("default")))
-const char *jeviterm_version(void) {
+__attribute__((visibility("default"))) void doit(void) {
+    const char *cmds[] = {"/usr/bin/env bash -l -c vi", "/usr/bin/env bash -l -c nano", nullptr};
+    jeviterm_open_tabs(cmds, "jevitermtest");
+}
+
+__attribute__((visibility("default"))) const char *jeviterm_version(void) {
     static std::string res;
     if (res.empty()) {
-        res += "jeviterm v " JEVITERM_VERSION "\n";
-        if(JEVITERM_GIT_RETRIEVED_STATE) {
-            if(JEVITERM_GIT_IS_DIRTY) res += "WARN: there were uncommitted changes.\n";
+        res += "jeviterm version " JEVITERM_VERSION "\n";
+        if (JEVITERM_GIT_RETRIEVED_STATE) {
+            if (JEVITERM_GIT_IS_DIRTY)
+                res += "WARN: there were uncommitted changes.\n";
 
             // Print information about the commit.
             // The format imitates the output from "git log".
-            res += "commit " JEVITERM_GIT_HEAD_SHA1 " (HEAD)\n" \
-                    "Describe: " JEVITERM_GIT_DESCRIBE "\n" \
-                    "Date: " JEVITERM_GIT_COMMIT_DATE_ISO8601;
+            res += "commit " JEVITERM_GIT_HEAD_SHA1 " (HEAD)\n"
+                   "Describe: " JEVITERM_GIT_DESCRIBE "\n"
+                   "Date: " JEVITERM_GIT_COMMIT_DATE_ISO8601;
         }
     }
     return res.c_str();
